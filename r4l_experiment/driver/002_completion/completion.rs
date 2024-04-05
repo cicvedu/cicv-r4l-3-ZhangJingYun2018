87db037b4d3df455f3ab7a2203506fda8f50ae5b
zjy <2826198807@qq.com>
2024-04-06 00:48:29 +0800

    优化

--- r4l_experiment/driver/002_completion/completion.rs
+++ r4l_experiment/driver/002_completion/completion.rs
@@ -3,11 +3,14 @@
 
 use core::result::Result::Err;
 
-use core::cell::UnsafeCell;
-use kernel::bindings;
 use kernel::prelude::*;
 use kernel::sync::Mutex;
 use kernel::{chrdev, file};
+use kernel::task::Task;
+
+mod completionops;
+use completionops::{CompletionFileOps};
+
 
 module! {
   type: Completion,
@@ -21,65 +24,10 @@ const GLOBALMEM_SIZE: usize = 0x1000;
 
 static GLOBALMEM_BUF: Mutex<[u8; GLOBALMEM_SIZE]> = unsafe { Mutex::new([0u8; GLOBALMEM_SIZE]) };
 
-static mut COMPLETION_FILE_OPS: CompletionFileOps = CompletionFileOps(None);
-
-struct CompletionFileOps(Option<UnsafeCell<bindings::completion>>);
-
-impl CompletionFileOps {
-    pub(crate) fn new() {
-        pr_info!("CompletionFile(new)\n");
-        unsafe {
-            COMPLETION_FILE_OPS.0 = Some(UnsafeCell::new(bindings::completion::default()));
-        }
-        pr_info!("CompletionFile(new){:?}\n", COMPLETION_FILE_OPS.0);
-    }
-
-    pub(crate) fn init_completion() {
-        unsafe {
-            if let Some(completion) = COMPLETION_FILE_OPS.0.as_mut() {
-                bindings::init_completion(completion.get());
-            }
-            pr_info!("CompletionFile(init_completion)\n");
-        }
-    }
-
-    pub(crate) fn wait_for_completion() {
-        unsafe {
-            if let Some(completion) = COMPLETION_FILE_OPS.0.as_mut() {
-                bindings::wait_for_completion(completion.get());
-            }
-            pr_info!("CompletionFile(wait_for_completion)\n");
-        }
-    }
-
-    pub(crate) fn complete() {
-        unsafe {
-            if let Some(completion) = COMPLETION_FILE_OPS.0.as_mut() {
-                bindings::complete(completion.get());
-            }
-            pr_info!("CompletionFile(complete)\n");
-        }
-    }
-}
-
-unsafe impl Send for CompletionFileOps {}
-unsafe impl Sync for CompletionFileOps {}
-
 struct CompletionFile {
     data: &'static Mutex<[u8; GLOBALMEM_SIZE]>,
 }
 
-struct Current {
-    task_struct: bindings::task_struct,
-}
-
-impl Current {
-    fn new() -> Self {
-        Self {
-            task_struct: unsafe { *Box::from_raw(bindings::get_current()) },
-        }
-    }
-}
 
 #[vtable]
 impl file::Operations for CompletionFile {
@@ -98,12 +46,9 @@ impl file::Operations for CompletionFile {
         reader: &mut impl kernel::io_buffer::IoBufferReader,
         _offset: u64,
     ) -> Result<usize> {
-        let current = Current::new();
-        let name = unsafe { CStr::from_char_ptr(current.task_struct.comm.as_ptr()) };
         pr_info!(
-            "CompletionFile -pid={:?}-pname={:?}-(write)\n",
-            current.task_struct.pid,
-            name
+            "CompletionFile -pid={:?}--(write)\n",
+            Task::current().pid(),
         );
         let mut globalmem = this.data.lock();
         pr_info!("CompletionFile(write-1)\n");
@@ -125,12 +70,9 @@ impl file::Operations for CompletionFile {
         if writer.is_empty() || offset > 0 {
             return Ok(0);
         }
-        let current = Current::new();
-        let name = unsafe { CStr::from_char_ptr(current.task_struct.comm.as_ptr()) };
         pr_info!(
-            "CompletionFile -pid={:?}-pname={:?}-(read)\n",
-            current.task_struct.pid,
-            name
+            "CompletionFile -pid={:?}--(read)\n",
+            Task::current().pid(),
         );
         CompletionFileOps::wait_for_completion();
         pr_info!("CompletionFile(read-1)\n");
@@ -145,16 +87,18 @@ impl file::Operations for CompletionFile {
 }
 
 struct Completion {
-    _dev: Pin<Box<chrdev::Registration<1>>>,
+    _dev: Pin<Box<chrdev::Registration<2>>>,
 }
 
 impl kernel::Module for Completion {
     fn init(name: &'static CStr, module: &'static ThisModule) -> Result<Self> {
         pr_info!("Completion driver demo (init)");
-        CompletionFileOps::new();
+        CompletionFileOps::new()?;
         CompletionFileOps::init_completion();
         let mut completion_reg = chrdev::Registration::new_pinned(name, 0, module)?;
         completion_reg.as_mut().register::<CompletionFile>()?;
+        completion_reg.as_mut().register::<CompletionFile>()?;
+
         Ok(Completion {
             _dev: completion_reg,
         })
@@ -164,8 +108,5 @@ impl kernel::Module for Completion {
 impl Drop for Completion {
     fn drop(&mut self) {
         pr_info!("Completion module is being unloaded now (exit)\n");
-        unsafe {
-            COMPLETION_FILE_OPS.0 = None;
-        }
     }
 }
---
+++ r4l_experiment/driver/002_completion/completionops.rs
@@ -0,0 +1,64 @@
+
+use core::cell::UnsafeCell;
+use kernel::bindings;
+use kernel::prelude::*;
+use kernel::sync::UniqueArc;
+
+pub(crate) struct CompletionFileOps(UnsafeCell<bindings::completion>);
+
+static mut COMPLETION_FILE_OPS: Option<Pin<UniqueArc<CompletionFileOps>>> = None;
+
+
+impl CompletionFileOps {
+    pub(crate) fn new() -> Result<()> {
+        pr_info!("CompletionFile(new)\n");
+        unsafe {
+            COMPLETION_FILE_OPS = Some(Pin::from(UniqueArc::try_new(CompletionFileOps(
+                UnsafeCell::new(bindings::completion::default()),
+            ))?));
+        }
+        Ok(())
+    }
+
+    pub(crate) fn init_completion() {
+        unsafe {
+            if let Some(completion) = &COMPLETION_FILE_OPS.as_mut() {
+                bindings::init_completion(completion.0.get());
+            }
+            pr_info!("CompletionFile(init_completion)\n");
+        }
+    }
+
+    pub(crate) fn wait_for_completion() {
+        unsafe {
+            if let Some(completion) = &COMPLETION_FILE_OPS.as_mut() {
+                bindings::wait_for_completion(completion.0.get());
+            }
+            pr_info!("CompletionFile(wait_for_completion)\n");
+        }
+    }
+
+    pub(crate) fn complete() {
+        unsafe {
+            if let Some(completion) = &COMPLETION_FILE_OPS.as_mut() {
+                bindings::complete(completion.0.get());
+            }
+            pr_info!("CompletionFile(complete)\n");
+        }
+    }
+}
+
+unsafe impl Send for CompletionFileOps {}
+unsafe impl Sync for CompletionFileOps {}
+
+// struct Current {
+//     task_struct: bindings::task_struct,
+// }
+
+// impl Current {
+//     fn new() -> Self {
+//         Self {
+//             task_struct: unsafe { *Box::from_raw(bindings::get_current()) },
+//         }
+//     }
+// }
